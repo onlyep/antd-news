c5183b7923bcb24bc56d8268122e3aa1b9a6de76
onlyep <250433177@qq.com>
2018-06-14 13:44:40 +0800

    router

--- src/components/pc_index.js
+++ src/components/pc_index.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import '../style/pc_index.less';
 import PCHeader from './pc_header';
 import PCFooter from './pc_footer';
+import ParamsExample from './router'
 
 export default class PCIndex extends Component {
 	render() {
 		return (
 			<div>
 				<PCHeader/>
+				<ParamsExample/>
 				<PCFooter/>
 			</div>
 		);
